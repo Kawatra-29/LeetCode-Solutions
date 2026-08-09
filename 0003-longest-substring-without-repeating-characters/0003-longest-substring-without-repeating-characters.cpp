f11a387777e65f1be43be2c00794ceeb237d22fe
SAURABH KAWATRA <saurabhkawatra2001@gmail.com>
2026-08-09 14:26:55 +0530

    Time: 65 ms (13.25%), Space: 17.8 MB (22.16%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s == "") {
+            return 0;
+        }
+        
+        // 1 r 1 t 7
+        string freq = "";
+        int x = 0, y = 0, len = 0, prev = 1;
+        while (y < s.size()) {
+            if (freq.find(s[y]) == string::npos) {
+                freq.push_back(s[y]);
+                y++;
+                len++;
+            } else {
+                if (len > prev) {
+                    prev = len;
+                }
+                len --;;
+                x++;
+                freq.erase(0, 1);
+            }
+        }
+        return max(prev, len);
+    }
+};
